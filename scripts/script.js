bb2f00c6091dfd11e43a9c983935a61634257151
Estasia Cai <ecai3@my.bcit.ca>
2019-03-26 20:45:44 -0700

    hamburger and menu script edits

--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-// Responsive Menu - Dropdown
-const body = document.body;
-const hamburger = document.getElementById('hamburger');
-
-hamburger.addEventListener('click', openMenu);
-
-function openMenu(){
-    body.classList.toggle('show');
-}
