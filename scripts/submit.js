a054daef3d5f16e8a070fdc1c0f46811cc0d7307
Naomi <naomitonegawa@gmail.com>
2019-04-01 23:09:37 -0700

    on click button done

--- scripts/submit.js
+++ scripts/submit.js
@@ -1,1 +1,15 @@
-// document.getElementById ("btn-submt").submit();    
+// document.getElementById ("btn-submt").submit();
+function dataEntryConfirmed() {
+
+    var submitButton = document.getElementById('btn-submit');
+
+    submitButton.onclick = alert("Thank you, your information has been sent!");
+}
+
+// Function validate(form) {
+
+// object.addEventListener("btn-submit", myScript);
+
+// onsubmit.alert("Thank you, your information has been sent!")
+// }
+
